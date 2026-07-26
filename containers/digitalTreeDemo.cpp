85acaef77dcbe8ec7603c166347b4e41ef21356a
quispedavid987 <quispedavid987@gmail.com>
2026-07-25 19:22:38 -0500

    last digitalTree

--- containers/digitalTreeDemo.cpp
+++ containers/digitalTreeDemo.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <string>
+#include <cstddef>
 
 using namespace std;
+using TI = int;
 
 struct TrieNode { // estructura de cada casilla
     TrieNode* children[26]; // 26 punteros... correspodiente a las letras del abecedario
     bool isEndOfWord;
 
     TrieNode() {
-        isEndOfWord = false; // nos asegurados que cada nuevo nodo no inicie marcado como final de palabra
-        for (int i = 0; i < 26; i++) {
+        isEndOfWord = false; // nos aseguramos que cada nuevo nodo no inicie marcado como final de palabra
+        for (size_t i = 0; i < 26; i++) {
             children[i] = nullptr;
         }
     }
@@ -35,15 +37,15 @@ private:
         cout << nodeChar << (node->isEndOfWord ? "*" : "") << "\n";
 
         // 3. Buscar cuál es el último hijo válido de este nodo
-        int lastChildIndex = -1;
-        for (int i = 0; i < 26; i++) {
+        TI lastChildIndex = -1;
+        for (size_t i = 0; i < 26; i++) {
             if (node->children[i] != nullptr) {
                 lastChildIndex = i;
             }
         }
-
+        
         // 4. Llamada recursiva para todos los hijos existentes
-        for (int i = 0; i < 26; i++) {
+        for (size_t i = 0; i < 26; i++) {
             if (node->children[i] != nullptr) {
                 // Pasamos 'true' si el índice actual coincide con el último hijo encontrado
                 printHelper(node->children[i], prefix, i == lastChildIndex, i + 'a');
@@ -59,7 +61,7 @@ public:
     void insert(string word) {
         TrieNode* curr = root;
         for (char c : word) {
-            int index = c - 'a'; // convierte el caracter c en un entero, 0 - 26
+            TI index = c - 'a'; // convierte el caracter c en un entero, 0 - 26
             if (curr->children[index] == nullptr) {
                 curr->children[index] = new TrieNode();
             }
@@ -73,15 +75,15 @@ public:
         cout << "(root)\n";
         
         // Determinar cuál es el último hijo directo de la raíz
-        int lastChildIndex = -1;
-        for (int i = 0; i < 26; i++) {
+        TI lastChildIndex = -1;
+        for (size_t i = 0; i < 26; i++) {
             if (root->children[i] != nullptr) {
                 lastChildIndex = i;
             }
         }
 
         // Iniciar la recursión para cada hijo de la raíz
-        for (int i = 0; i < 26; i++) {
+        for (size_t i = 0; i < 26; i++) {
             if (root->children[i] != nullptr) {
                 printHelper(root->children[i], "", i == lastChildIndex, i + 'a');
             }
@@ -105,7 +107,7 @@ int main() {
     // Imprimimos el árbol
     miTrie->print();
 
-    // Liberación de memoria (buena práctica en C++)
+    // Liberación de memoria 
     delete miTrie; 
     return 0;
 }
